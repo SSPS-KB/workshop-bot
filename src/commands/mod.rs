eb0d399177d601cc7e9a73869a9a22977e073a7c
Marek Vospěl <marek@vospel.cz>
2023-02-05 17:17:51 +0100

    fix: register commands only globally

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -13,17 +13,41 @@ pub(crate) async fn register_commands(ctx: &Context) {
         "{:?}",
         guild_id
             .set_application_commands(&ctx.http, |commands| {
+                /*
                 commands.create_application_command(|command| otakugif::register_kiss(command));
                 commands.create_application_command(|command| otakugif::register_hug(command));
+                */
                 commands
             })
             .await
     );
 
-    match Command::create_global_application_command(&ctx.http, |command| {
-        workshop::register(command)
-    })
-    .await
+    let mut results = Vec::new();
+
+    results.push(
+        Command::create_global_application_command(&ctx.http, |command| {
+            workshop::register(command)
+        })
+        .await,
+    );
+
+    results.push(
+        Command::create_global_application_command(&ctx.http, |command| {
+            otakugif::register_kiss(command)
+        })
+        .await,
+    );
+
+    results.push(
+        Command::create_global_application_command(&ctx.http, |command| {
+            otakugif::register_hug(command)
+        })
+        .await,
+    );
+
+    match results
+        .into_iter()
+        .collect::<Result<Vec<Command>, serenity::Error>>()
     {
         Ok(_) => info!("Created global application commands"),
         Err(e) => error!("Could not create global application command: {}", e),
