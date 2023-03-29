ea14a6c53fbc225cc2e2d663c200cc7f97197804
petulka <axmyoxd@gmail.com>
2023-03-29 21:52:11 +0200

    zkouzka?

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -11,6 +11,7 @@ pub(crate) mod reaction_role;
 pub(crate) mod skull;
 pub(crate) mod workshop;
 pub(crate) mod cat;
+pub(crate) mod amimir;
 
 pub(crate) async fn register_commands(ctx: &Context) {
     let guild_id = GuildId::from(1069606131510562889);
