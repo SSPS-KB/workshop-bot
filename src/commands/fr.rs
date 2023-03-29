a5abfe868cc907c03b940a5ae4f6744c394b4022
tastefulblatant <121983707+tastefulblatant@users.noreply.github.com>
2023-03-29 21:14:05 +0200

    added /fr command

---
+++ src/commands/fr.rs
@@ -0,0 +1,48 @@
+use serenity::builder::CreateApplicationCommand;
+use serenity::model::prelude::interaction::application_command::ApplicationCommandInteraction;
+use serenity::model::prelude::interaction::InteractionResponseType;
+use serenity::prelude::Context;
+use serenity::utils::Color;
+use tracing::error;
+use i18n::t;
+
+pub(crate) fn register(
+    command: &mut CreateApplicationCommand,
+) -> &mut CreateApplicationCommand {
+    command
+        .name("fr")
+        .description(t!("commands.fr.description").to_string())
+        .description_localized("cs", t!("commands.fr.description", "cs"))
+        .dm_permission(true)
+}
+
+fn get_message(locale: String) -> &'static str {
+    match locale.as_str() {
+        "cs" => t!("commands.fr.message", "cs"),
+        _ => t!("commands.fr.message"),
+    }
+}
+
+pub(crate) async fn run(ctx: &Context, command: &ApplicationCommandInteraction) {
+    let locale = command.clone().locale;
+
+    if let Err(e) = command
+        .create_interaction_response(&ctx.http, |response| {
+            response
+                .kind(InteractionResponseType::ChannelMessageWithSource)
+                .interaction_response_data(|message| {
+                    message
+                        .content(get_message(locale))
+                        .embed(|embed| embed
+                            .image("https://tenor.com/view/bacoco-cat-cats-fr-fr-ong-ong-gif-25098850")
+                            .color(Color::from_rgb(110, 110, 110)))
+                })
+        })
+        .await
+    {
+        error!(
+            "There was an error while responding to chad command: {}",
+            e
+        )
+    };
+}
