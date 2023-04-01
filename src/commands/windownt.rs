9f38da163947df30135101aa795a315e7705f14e
petulka <axmyoxd@gmail.com>
2023-04-01 17:44:33 +0200

    Create windownt.rs

---
+++ src/commands/windownt.rs
@@ -0,0 +1,48 @@
+use serenity::builder::CreateApplicationCommand;
+use serenity::model::prelude::interaction::application_command::ApplicationCommandInteraction;
+use serenity::model::prelude::interaction::InteractionResponseType;
+use serenity::prelude::Context;
+use serenity::utils::Color;
+use tracing::error;
+use i18n::t;
+
+pub(crate) fn register(
+    command: &mut CreateApplicationCommand,
+) -> &mut CreateApplicationCommand {
+    command
+        .name("windownt")
+        .description(t!("commands.windownt.description").to_string())
+        .description_localized("cs", t!("commands.windownt.description", "cs"))
+        .dm_permission(true)
+}
+
+fn get_message(locale: String) -> &'static str {
+    match locale.as_str() {
+        "cs" => t!("commands.windownt.message", "cs"),
+       _ => t!("commands.windownt.message"),
+    }
+}
+
+pub(crate) async fn run(ctx: &Context, command: &ApplicationCommandInteraction) {
+    let locale = command.clone().locale;
+
+    if let Err(e) = command
+        .create_interaction_response(&ctx.http, |response| {
+            response
+                .kind(InteractionResponseType::ChannelMessageWithSource)
+                .interaction_response_data(|message| {
+                    message
+                        .content(get_message(locale))
+                        .embed(|embed| embed
+                            .image("https://i.imgur.com/a/xk9W3fy")
+                            .color(Color::from_rgb(110, 110, 110)))
+                })
+        })
+        .await
+    {
+        error!(
+            "There was an error while responding to windownt command: {}",
+            e
+        )
+    };
+}
