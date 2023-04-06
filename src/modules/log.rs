452857cc566bcf8b545945417b6b8180fc83774c
petulka <axmyoxd@gmail.com>
2023-04-06 23:05:05 +0200

    cc

--- src/modules/log.rs
+++ src/modules/log.rs
@@ -7,14 +7,13 @@ pub struct MessageDeleteLog;
 
 impl EventHandler for MessageDeleteLog {
     fn message_delete(&self, ctx: Context, channel_id: ChannelId, message_id: MessageId) {
-        // check if the message is from the desired channel ID
         if channel_id != ChannelId(01) {
             return;
         }
 
-        // create an embed for the message deletion log
+
         let embed = EmbedBuilder::new()
-            .color(0xff0000) // red color for the embed
+            .color(0xff0000) 
             .title("Message Deleted")
             .field("Message ID", message_id.to_string(), false)
             .field("Channel ID", channel_id.to_string(), false)
@@ -23,7 +22,7 @@ impl EventHandler for MessageDeleteLog {
             .timestamp(message_id.timestamp())
             .build();
 
-        // send the embed to the desired channel ID
+ 
         if let Err(why) = ChannelId(01).send_message(&ctx.http, |m| {
             m.embed(|e| {
                 e.clone_from(embed.as_ref().unwrap());
@@ -39,12 +38,11 @@ pub struct MessageEditLog;
 
 impl EventHandler for MessageEditLog {
     fn message_update(&self, ctx: Context, old_message: Option<Message>, new_message: Message, _event_type: MessageUpdateEvent) {
-        // check if the message is from the desired channel ID
         if new_message.channel_id != ChannelId(01) {
             return;
         }
 
-        // create an embed for the message edit log
+        
         let mut fields = vec![
             ("Message ID", new_message.id.to_string(), false),
             ("Channel ID", new_message.channel_id.to_string(), false),
@@ -60,13 +58,13 @@ impl EventHandler for MessageEditLog {
         }
 
         let embed = EmbedBuilder::new()
-            .color(0x0000ff) // blue color for the embed
+            .color(0x0000ff) 
             .title("Message Edited")
             .fields(fields)
             .timestamp(new_message.timestamp)
             .build();
 
-        // send the embed to the desired channel ID
+        
         if let Err(why) = ChannelId(01).send_message(&ctx.http, |m| {
             m.embed(|e| {
                 e.clone_from(embed.as_ref().unwrap());
